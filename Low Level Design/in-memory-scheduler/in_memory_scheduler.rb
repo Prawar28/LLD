05577dd769a52751e1de1e0650d6871acc5e2da7
Prawar28 <53871904+Prawar28@users.noreply.github.com>
2024-02-14 19:56:52 +0530

    Update in_memory_scheduler.rb

--- Low Level Design/in-memory-scheduler/in_memory_scheduler.rb
+++ Low Level Design/in-memory-scheduler/in_memory_scheduler.rb
@@ -1,18 +1,29 @@
 require 'thread'
 
+# we could also pas queue_name and have separate queues as per requirement
+# we could have different scheduler threads for one time and interval one
+# we could separately store 
 class JobScheduler
     def initialize(num_of_threads = 5)
         @job_queue = Queue.new # thread safe queue
         @mutex = Mutex.new # to ensure two threads do not pop at same time
         @num_of_threads = num_of_threads
-        @interval_threads = []
         @condition = ConditionVariable.new
+        @worker_threads = []
+        @scheduler_thread = Thread.new { schedule_jobs }
+        @all_jobs = []
+    end
+
+    def start_workers
         @worker_threads = []
         @num_of_threads.times do
             @worker_threads << Thread.new { worker_execute }
         end
-        @scheduler_thread = Thread.new { schedule_jobs }
-        @all_jobs = []
+    end
+
+    def stop_workers
+        @worker_threads.each(&:exit)
+        @scheduler_thread.exit()
     end
 
     def schedule_at(task, time)
@@ -35,12 +46,19 @@ class JobScheduler
                 if job[:time]
                     if (job[:time] - Time.now <= 0)
                         @mutex.synchronize do
-                            @job_queue << { task: task, time: time }
+                            @job_queue << job
                             @condition.signal
                         end
                         @all_jobs.delete(job)
+                    end
                 elsif job[:interval]
-                    
+                    if !job[:last_executed_at] || (job[:last_executed_at] + job[:interval] <= Time.now)
+                        @mutex.synchronize do
+                            @job_queue << job
+                            @condition.signal
+                        end
+                        job[:last_executed_at] = Time.now
+                    end
                 end
             end
             sleep(0.1)
