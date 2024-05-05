7af8cff4785a7d9bbcc6192bb6df620a41eba886
Prawar28 <53871904+Prawar28@users.noreply.github.com>
2024-05-05 22:20:57 +0530

    Delete Low Level Design directory

--- Low Level Design/rate-limiter-leaky-bucket/run_script.rb
+++
@@ -1,7 +0,0 @@
-load './application.rb'
-
-application = Application.new
-
-(1..5).each do |i|
-    application.try_access(1)
-end
--- Low Level Design/rate-limiter-token-bucket/run_script.rb
+++
@@ -1,8 +0,0 @@
-load './application.rb'
-
-application = Application.new
-
-(1..5).each do |i|
-    application.try_access(1)
-    sleep(2) if i == 3
-end
